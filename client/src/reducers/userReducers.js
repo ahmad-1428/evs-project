1610609b824e81c834ebfe61f16cf90a5edfb828
Ahmed Nadeem <139245533+ahmad-1428@users.noreply.github.com>
2026-02-14 19:50:57 +0500

    Update userReducers.js

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -1,19 +1,18 @@
-// reducers/userReducers.js
-export const userUpdateProfileReducer = (state = {}, action) => {
-    switch (action.type) {
-      case USER_UPDATE_PROFILE_REQUEST:
-        return { loading: true };
-      case USER_UPDATE_PROFILE_FAIL:
-        return { loading: false, error: action.payload };
-        case USER_UPDATE_PROFILE_SUCCESS:
-  return {
-    loading: false,
-    success: true,
-    userInfo: action.payload,
-  };
-
-      default:
-        return state;
-    }
-  };
-  
+// Inside your authReducer (or wherever userInfo is managed)
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'USER_LOGIN_REQUEST':
+      return { loading: true };
+    case 'USER_LOGIN_SUCCESS':
+      return { loading: false, userInfo: action.payload };
+    // ADD THIS CASE BELOW:
+    case 'USER_UPDATE_PROFILE_SUCCESS':
+      return { loading: false, userInfo: action.payload }; 
+    case 'USER_LOGIN_FAIL':
+      return { loading: false, error: action.payload };
+    case 'USER_LOGOUT':
+      return {};
+    default:
+      return state;
+  }
+};
