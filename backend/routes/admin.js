2eb4cc271a653033ad57cd853a5fd2265801f144
Ahmed Nadeem <139245533+ahmad-1428@users.noreply.github.com>
2026-02-14 19:47:24 +0500

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,21 +1,11 @@
-import express from "express";
-import {
-  getOrders,
-  getProducts,
-  getUsers,
-  createProduct,
-  getProduct,
-  updateProduct,
-} from "../controller/admin.js";
-import adminHandler from "../middlewares/adminHandler.js";
+import express from 'express';
+import { protect, admin } from '../middlewares/authMiddleware.js';
+import { getUsers, getOrders, getProducts } from '../controller/admin.js';  // Updated: Correct path to controller/admin.js (singular)
+
 const router = express.Router();
 
-router.get("/orders", adminHandler, getOrders);
-router.get("/products", adminHandler, getProducts);
-router.get("/users", adminHandler, getUsers);
-
-router.get("/products/:id", adminHandler, getProduct);
-router.post("/products", adminHandler, createProduct);
-router.put("/products/:id", adminHandler, updateProduct);
+router.get('/users', protect, admin, getUsers);
+router.get('/orders', protect, admin, getOrders);
+router.get('/products', protect, admin, getProducts);
 
 export default router;
